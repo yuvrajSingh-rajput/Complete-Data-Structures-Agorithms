7adbd962e2489d0001a60e5e17cc7428b20b9c2a
yuvraj_singh_rajput <161643356+yuvrajSingh-rajput@users.noreply.github.com>
2024-06-12 11:18:20 +0530

    Update sortColors.cpp

--- Algorithms/DutchNationalFlag & derivatives./sortColors.cpp
+++ Algorithms/DutchNationalFlag & derivatives./sortColors.cpp
@@ -1,1 +1,40 @@
-/*Hello*/
+/*
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+*/
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int start = 0;
+        int end = nums.size()-1;
+        int mid = 0;
+        while(mid<=end){
+            
+            if(nums[mid] ==0){
+                swap(nums[start],nums[mid]);
+                start ++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[end]);
+                end--;
+            }
+        }
+    }
+};
