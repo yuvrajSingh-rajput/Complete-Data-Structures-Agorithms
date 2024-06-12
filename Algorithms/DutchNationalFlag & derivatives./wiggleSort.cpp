7f66f4b30c65ca670e2492b48b49cb569a584c5d
yuvraj_singh_rajput <161643356+yuvrajSingh-rajput@users.noreply.github.com>
2024-06-12 11:16:47 +0530

    Create wiggleSort.cpp

---
+++ Algorithms/DutchNationalFlag & derivatives./wiggleSort.cpp
@@ -0,0 +1,34 @@
+/*
+Given an integer array nums, reorder it such that nums[0] < nums[1] > nums[2] < nums[3]....
+
+You may assume the input array always has a valid answer.
+
+Example 1:
+
+Input: nums = [1,5,1,1,6,4]
+Output: [1,6,1,5,1,4]
+Explanation: [1,4,1,5,1,6] is also accepted.
+Example 2:
+
+Input: nums = [1,3,2,2,3,1]
+Output: [2,3,1,3,1,2]
+*/
+
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        vector<int> sorted(nums);
+        sort(sorted.begin(), sorted.end());
+        int n = nums.size();
+        int mid = (n + 1) / 2;
+        int j = mid - 1, k = n - 1;
+
+        for (int i = 0; i < n; ++i) {
+            if (i % 2 == 0) {
+                nums[i] = sorted[j--];
+            } else {
+                nums[i] = sorted[k--];
+            }
+        }
+    }
+};
