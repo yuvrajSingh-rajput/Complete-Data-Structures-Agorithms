d8f31d8d7f7982ded6c2a06f987f366a70c3050b
yuvraj_singh_rajput <161643356+yuvrajSingh-rajput@users.noreply.github.com>
2024-08-13 19:47:58 +0530

    Add files via upload

---
+++ GRAPHS/problems on DFS BFS/word_Ladder_I.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> st(wordList.begin(), wordList.end());
+        
+        queue<pair<string, int>> q;
+        q.push({beginWord, 1});
+        st.erase(beginWord);
+
+        while(!q.empty()){
+            string word = q.front().first;
+            int steps = q.front().second;
+            q.pop();
+
+            if(word == endWord) return steps;
+
+            for(int i = 0; i < word.length(); i++){
+                char original = word[i];
+                for(char ch = 'a'; ch <= 'z'; ch++){
+                    word[i] = ch;
+                    if(st.find(word) != st.end()){
+                        q.push({word, steps + 1});
+                        st.erase(word);
+                    }
+                }
+                word[i] = original;
+            }
+        }
+        return 0;
+    }
+};
+
+signed main(int argc, char* argv[]){
+    ios_base::sync_with_stdio(false);
+    #ifndef YUVRAJ_SINGH_RAJPUT_DEBUG
+        cin.tie(nullptr);
+        auto _clock_start = chrono::high_resolution_clock::now();
+    #endif
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    
+
+    #ifndef YUVRAJ_SINGH_RAJPUT_DEBUG
+        auto _clock_end = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::milliseconds>(_clock_end-_clock_start).count();
+        cerr << "Execution time: " << duration << " ms." << endl;
+    #endif
+
+    return 0;
+}
