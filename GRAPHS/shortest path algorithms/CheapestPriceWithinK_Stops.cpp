970f801a69200023ef015453193beee3d8c6b2f7
yuvraj_singh_rajput <161643356+yuvrajSingh-rajput@users.noreply.github.com>
2024-08-26 16:02:34 +0530

    Update CheapestPriceWithinK_Stops.cpp

--- GRAPHS/shortest path algorithms/CheapestPriceWithinK_Stops.cpp
+++ GRAPHS/shortest path algorithms/CheapestPriceWithinK_Stops.cpp
@@ -1,6 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//Method 1: Normal BFS approach->
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int,int>>>adj(n);
+        for(auto it:flights){
+           adj[it[0]].push_back({it[1],it[2]});
+
+        }
+        
+        vector<int>price(n,INT_MAX);
+        price[src]=0;
+        queue<pair<int,pair<int,int>>>q;
+        q.push({0,{src,0}});
+        while(!q.empty()){
+            int stop=q.front().first;
+            int node=q.front().second.first;
+            int cost=q.front().second.second;
+            q.pop();
+           
+            if(stop>k){
+               continue; 
+            }
+             
+            for(auto it:adj[node]){
+             
+              if(stop<=k&&cost+it.second<price[it.first]){
+                 price[it.first]=cost+it.second;
+                  q.push({stop+1,{it.first,cost+it.second}});
+              }
+            }
+        }
+        if(price[dst]==INT_MAX)return -1;
+        return price[dst];
+    }
+};
+
+//Method 2: Dynamic Programming Approach->
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
