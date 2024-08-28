18ad08be69fb4994c0555795e7cf6cb51bd1564e
yuvraj_singh_rajput <161643356+yuvrajSingh-rajput@users.noreply.github.com>
2024-08-28 19:34:10 +0530

    Create FloydWarshalls_Algorithm.cpp

---
+++ GRAPHS/shortest path algorithms/FloydWarshalls_Algorithm.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+	void shortest_distance(vector<vector<int>>&matrix){
+	    int n = matrix.size();
+	    for(int i = 0; i < n; i++){
+	        for(int j = 0; j < n; j++){
+	            if(matrix[i][j] == -1) matrix[i][j] = 1e9;
+	            if(i == j) matrix[i][j] = 0;
+	        }
+	    }
+	    // Floyd Warshall's Algorithm 
+	    for(int k = 0; k < n; k++){
+	        for(int i = 0; i < n; i++){
+	            for(int j = 0; j < n; j++){
+	                matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+	            }
+	        }
+	    }
+	    
+	    // if negative weight cycle exists then do a check
+	    for(int i = 0; i < n; i++){
+	        if(matrix[i][i] < 0) return;
+	    }
+	    
+	    for(int i = 0; i < n; i++){
+	        for(int j = 0; j < n; j++){
+	            if(matrix[i][j] == 1e9) matrix[i][j] = -1;
+	        }
+	    }
+	}
+};
